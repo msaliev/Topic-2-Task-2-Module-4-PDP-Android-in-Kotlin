bb145532524e037ee0c28b5c537329be4af79402
Muhammad Yusuf <farmax298@gmail.com>
2022-01-27 18:39:23 +0500

    Task 2 in Kotlin

---
+++ app/src/main/java/com/example/twitterclonehwinkotlin/activities/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.twitterclonehwinkotlin.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.twitterclonehwinkotlin.R
+
+class SplashActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            val i = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(i)
+            finish()
+        }, 2000)
+
+    }
+}
